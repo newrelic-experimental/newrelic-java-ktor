3a2fea0f9ccefcd0d2ec4cb07519ea8f19d15186
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2024-08-01 13:59:10 -0500

    fixed transaction naming and problems related to tracking async behavior

--- ktor-server-core-1.x/src/main/java/io/ktor/routing/Route.java
+++ ktor-server-core-1.x/src/main/java/io/ktor/routing/Route.java
@@ -1,13 +1,12 @@
-package io.ktor.server.routing;
+package io.ktor.routing;
 
 import java.util.List;
 
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-import com.newrelic.instrumentation.labs.ktor.server.NRFunction3Wrapper;
 
-import io.ktor.server.application.ApplicationCall;
+import io.ktor.application.ApplicationCall;
 import io.ktor.util.pipeline.PipelineContext;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
@@ -17,18 +16,16 @@ import kotlin.jvm.functions.Function3;
 @Weave
 public abstract class Route {
 
-	@Trace(dispatcher = true)
+	@Trace
 	public void invoke(Function1<? super Route,Unit> f) {
 		Weaver.callOriginal();
 	}
 
 	@Trace
 	public void handle(Function3<? super PipelineContext<Unit, ApplicationCall>, ? super Unit, ? super Continuation<? super Unit>, ? extends Object> f) {
-		NRFunction3Wrapper<? super PipelineContext<Unit, ApplicationCall>, ? super Unit, ? super Continuation<? super Unit>, ? extends Object>  wrapper = new NRFunction3Wrapper<>(f,this);
-		f = wrapper;
 		Weaver.callOriginal();
 	}
-	
+
 	public abstract RouteSelector getSelector();
 	public abstract Route getParent();
 	public abstract List<Route> getChildren();
