679dd86dbb93a35e1a9740d9ecc2da0f5141e901
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2024-08-01 14:00:56 -0500

    fixed transaction naming and problems related to tracking async behavior

--- ktor-server-netty-2.0/src/main/java/io/ktor/server/netty/NettyApplicationCallHandler.java
+++ ktor-server-netty-2.0/src/main/java/io/ktor/server/netty/NettyApplicationCallHandler.java
@@ -1,9 +1,8 @@
 package io.ktor.server.netty;
 
-import java.util.logging.Level;
-
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.Transaction;
 import com.newrelic.api.agent.TransactionNamePriority;
 import com.newrelic.api.agent.TransportType;
 import com.newrelic.api.agent.weaver.Weave;
@@ -22,19 +21,21 @@ public class NettyApplicationCallHandler {
 
 	@Trace
 	private void handleRequest(ChannelHandlerContext ctx, ApplicationCall call) {
+		Transaction transaction = NewRelic.getAgent().getTransaction();
+		if(!transaction.isWebTransaction()) {
+			transaction.convertToWebTransaction();
+		}
 		ApplicationRequest request = call.getRequest();
 		if(request != null) {
 			Headers headers = request.getHeaders();
-			NewRelic.getAgent().getLogger().log(Level.FINE, "Got headers: {0}", headers);
 			if(headers != null) {
 				KtorNettyHeaders nrHeaders = new KtorNettyHeaders(headers);
-				NewRelic.getAgent().getTransaction().acceptDistributedTraceHeaders(TransportType.HTTP, nrHeaders);
+				transaction.acceptDistributedTraceHeaders(TransportType.HTTP, nrHeaders);
 			}
 			RequestConnectionPoint point = request.getLocal();
 			String txnName = Utils.getTransactionName(point);
-			NewRelic.getAgent().getLogger().log(Level.FINE, "Ktor transaction name: {0}", txnName);
 			if(txnName != null && !txnName.isEmpty()) {
-				NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.CUSTOM_HIGH, true, "KtorNetty", txnName);
+				transaction.setTransactionName(TransactionNamePriority.CUSTOM_LOW, false, "KtorNetty", txnName);
 			}
 		}
 		Weaver.callOriginal();
