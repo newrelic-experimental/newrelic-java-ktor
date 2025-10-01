15ef74cffd348e8e140597d5ae6789217a130440
Doug Hilpipre <dhilpipre@newrelic.com>
2025-10-01 08:17:32 -0500

    updated README and fixed verify

---
+++ ktor-server-core-1.2.3/src/main/java/io/ktor/request/ApplicationReceiveFunctionsKt$receive$2_Skip.java
@@ -0,0 +1,7 @@
+package io.ktor.request;
+
+import com.newrelic.api.agent.weaver.SkipIfPresent;
+
+@SkipIfPresent(originalName = "io.ktor.request.ApplicationReceiveFunctionsKt$receive$2")
+public class CannotTransformToTypeException_Skip {
+}
