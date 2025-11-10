36ec205b34192c72a4e3eb952f138903dbb47a04
gsidhwani-nr <gsidhwani@newrelic.com>
2025-11-10 17:02:07 +0530

    feat: added ktor-server-jetty instrumentation
    fix: added centralized build number control in build.gradle

---
+++ ktor-server-jetty/src/main/java/com/newrelic/labs/instrumentation/ktor/jetty/Utils.java
@@ -0,0 +1,47 @@
+package com.newrelic.labs.instrumentation.ktor.jetty;
+
+import io.ktor.server.application.Application;
+
+public class Utils {
+    
+    public static String getApplicationName(Application app) {
+        if (app != null) {
+            return app.getClass().getSimpleName();
+        }
+        return "Unknown";
+    }
+    
+    public static String getTransactionName(String uri, String method) {
+        StringBuilder sb = new StringBuilder();
+        
+        if (uri != null) {
+            if (uri.startsWith("/")) {
+                uri = uri.substring(1);
+            }
+            if (uri.isEmpty()) {
+                sb.append("Root");
+            } else {
+                sb.append(uri);
+            }
+        } else {
+            sb.append("Unknown");
+        }
+        
+        if (method != null && !method.isEmpty()) {
+            sb.append(" - {");
+            sb.append(method);
+            sb.append("}");
+        }
+        
+        return sb.toString();
+    }
+    
+    public static String getTransactionNameFromConnectionPoint(Object connectionPoint) {
+        // For now, return a simple transaction name based on connection point
+        // This would need to be enhanced based on actual RequestConnectionPoint implementation
+        if (connectionPoint != null) {
+            return connectionPoint.toString();
+        }
+        return "JettyConnection";
+    }
+}
---
+++ ktor-server-jetty/src/main/java/io/ktor/server/jetty/JettyApplicationCall_Instrumentation.java
@@ -0,0 +1,19 @@
+package io.ktor.server.jetty;
+
+import com.newrelic.api.agent.Token;
+import com.newrelic.api.agent.weaver.NewField;
+import com.newrelic.api.agent.weaver.Weave;
+import io.ktor.server.application.Application;
+import org.eclipse.jetty.server.Request;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import kotlin.coroutines.CoroutineContext;
+
+@Weave(originalName = "io.ktor.server.jetty.JettyApplicationCall")
+public class JettyApplicationCall_Instrumentation {
+
+    
+    public JettyApplicationCall_Instrumentation(Application application, Request request, HttpServletRequest servletRequest, HttpServletResponse servletResponse, CoroutineContext engineContext, CoroutineContext userContext, CoroutineContext coroutineContext) {
+        
+    }
+}
