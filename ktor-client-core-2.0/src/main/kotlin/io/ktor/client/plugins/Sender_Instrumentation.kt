1dd81fc5b8d49821a87973fa00490175f33aba5c
gsidhwani-nr <gsidhwani@newrelic.com>
2025-11-28 13:33:55 +0530

    fix: separted headerwrapper

--- ktor-client-core-2.0/src/main/kotlin/io/ktor/client/plugins/Sender_Instrumentation.kt
+++ ktor-client-core-2.0/src/main/kotlin/io/ktor/client/plugins/Sender_Instrumentation.kt
@@ -1,7 +1,5 @@
 package io.ktor.client.plugins
 
-import com.newrelic.api.agent.HeaderType
-import com.newrelic.api.agent.Headers
 import com.newrelic.api.agent.NewRelic
 import com.newrelic.api.agent.Trace
 import com.newrelic.api.agent.weaver.MatchType
@@ -9,6 +7,7 @@ import com.newrelic.api.agent.weaver.Weave
 import com.newrelic.api.agent.weaver.Weaver
 import io.ktor.client.call.HttpClientCall
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.plugins.KtorHeaderWrapper
 
 @Weave(type = MatchType.Interface, originalName = "io.ktor.client.plugins.Sender")
 class Sender_Instrumentation {
@@ -18,25 +17,7 @@ class Sender_Instrumentation {
         NewRelic.getAgent().tracedMethod.setMetricName("Custom","Ktor-Client","Sender",this.javaClass.name,"execute")
 
         // Add Distributed Tracing headers to the mutable request builder
-        val headersWrapper = object : Headers {
-            override fun getHeaderType(): HeaderType = HeaderType.HTTP
-
-            override fun getHeader(name: String): String? = requestBuilder.headers[name]
-
-            override fun getHeaders(name: String): Collection<String> = requestBuilder.headers.getAll(name) ?: emptyList()
-
-            override fun setHeader(name: String, value: String) {
-                requestBuilder.headers.append(name, value)
-            }
-
-            override fun addHeader(name: String, value: String) {
-                requestBuilder.headers.append(name, value)
-            }
-
-            override fun getHeaderNames(): Collection<String> = requestBuilder.headers.names()
-
-            override fun containsHeader(name: String): Boolean = requestBuilder.headers.contains(name)
-        }
+        val headersWrapper = KtorHeaderWrapper(requestBuilder.headers)
 
         // Insert DT headers before the request is sent to the engine
         NewRelic.getAgent().transaction.insertDistributedTraceHeaders(headersWrapper)
