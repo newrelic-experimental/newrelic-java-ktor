67b99b5388747706b1affd1e39b38b61e53a4419
Doug Hilpipre <dhilpipre@newrelic.com>
2025-12-08 12:23:25 -0600

    added null check to Sender to avoid errors

--- ktor-client-core-2.0/src/main/kotlin/io/ktor/client/plugins/Sender_Instrumentation.kt
+++ ktor-client-core-2.0/src/main/kotlin/io/ktor/client/plugins/Sender_Instrumentation.kt
@@ -8,6 +8,7 @@ import com.newrelic.api.agent.weaver.Weaver
 import io.ktor.client.call.HttpClientCall
 import io.ktor.client.request.HttpRequestBuilder
 import com.newrelic.instrumentation.labs.ktor.client.KtorHeaderWrapper
+import io.ktor.http.HttpMethod
 
 @Weave(type = MatchType.Interface, originalName = "io.ktor.client.plugins.Sender")
 class Sender_Instrumentation {
@@ -16,11 +17,14 @@ class Sender_Instrumentation {
     public suspend fun execute(requestBuilder: HttpRequestBuilder): HttpClientCall {
         NewRelic.getAgent().tracedMethod.setMetricName("Custom","Ktor-Client","Sender",this.javaClass.name,"execute")
 
-        // Add Distributed Tracing headers to the mutable request builder
-        val headersWrapper = KtorHeaderWrapper(requestBuilder.headers)
+        // in spite of what IntelliJ says this can be null
+        if (requestBuilder != null) {
+            // Add Distributed Tracing headers to the mutable request builder
+            val headersWrapper = KtorHeaderWrapper(requestBuilder.headers)
 
-        // Insert DT headers before the request is sent to the engine
-        NewRelic.getAgent().transaction.insertDistributedTraceHeaders(headersWrapper)
+            // Insert DT headers before the request is sent to the engine
+            NewRelic.getAgent().transaction.insertDistributedTraceHeaders(headersWrapper)
+        }
 
         return Weaver.callOriginal()
     }
