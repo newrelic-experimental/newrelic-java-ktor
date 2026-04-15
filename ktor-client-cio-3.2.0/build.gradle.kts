870d5882b8acf0e64728fdfb0a88c7fbd9cc9d88
Doug Hilpipre <dhilpipre@newrelic.com>
2026-04-15 13:48:35 -0500

    merged

--- ktor-client-cio-3.2.0/build.gradle.kts
+++ ktor-client-cio-3.2.0/build.gradle.kts
@@ -1,17 +1,9 @@
-<<<<<<<< HEAD:ktor-client-cio-3.2.0/build.gradle.kts
-// Build.gradle generated for instrumentation module ktor-client-cio
-========
-// Build.gradle generated for instrumentation module ktor-ignores-setup
->>>>>>>> main:ktor-ignores-setup/build.gradle.kts
 
 apply(plugin = "java")
 
 dependencies {
-<<<<<<<< HEAD:ktor-client-cio-3.2.0/build.gradle.kts
     implementation("io.ktor:ktor-client-cio-jvm:3.2.0")
-========
 
->>>>>>>> main:ktor-ignores-setup/build.gradle.kts
     // New Relic Java Agent dependencies
     implementation("com.newrelic.agent.java:newrelic-agent:9.1.0")
     implementation("com.newrelic.agent.java:newrelic-api:9.1.0")
@@ -24,18 +16,10 @@ dependencies {
 tasks.jar {
     manifest {
         attributes(
-<<<<<<<< HEAD:ktor-client-cio-3.2.0/build.gradle.kts
             "Implementation-Title" to "com.newrelic.instrumentation.labs.ktor-client-cio-3.2.0",
             "Implementation-Vendor" to "New Relic Labs",
             "Implementation-Vendor-Id" to "com.newrelic.labs",
             "Implementation-Version" to 1.0
-========
-            "Implementation-Title" to "com.newrelic.instrumentation.labs.ktor-ignores-setup",
-            "Implementation-Vendor" to "New Relic Labs",
-            "Implementation-Vendor-Id" to "com.newrelic.labs",
-            "Implementation-Version" to 1.0,
-            "Agent-Class" to "com.newrelic.instrumentation.labs.ktor.ignores.KtorSetupPremain"
->>>>>>>> main:ktor-ignores-setup/build.gradle.kts
         )
     }
 }
